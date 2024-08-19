23123544a33ea0d866ab5430036b75a920d03816
Camilito <camilo.padilla@ucb.edu.bo>
2024-08-18 22:37:32 -0400

    Saludar español/ingles

--- src/presenter.js
+++ src/presenter.js
@@ -4,44 +4,77 @@ const gender = document.querySelector("#gender")
 const age = document.querySelector("#age")
 const form = document.querySelector("#greeting-form");
 const div = document.querySelector("#Saludo-hola-div");
+const espButton = document.querySelector("#esp-button");
+const engButton = document.querySelector("#eng-button");
+
+let isEspañol = false;
+
+espButton.addEventListener("click", () => {
+  isEspañol = true;
+  updateGreeting();
+});
+
+engButton.addEventListener("click", () => {
+  isEspañol = false;
+  updateGreeting();
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  updateGreeting();
+});
 
+function updateGreeting()
+{
   let saludo = "Hola "
-  let user_Name = name.value
+  let user_Name = name.value.trim()
+  let user_age = parseInt(age.value)
   
-  if (gender.value === "masculino")
-  {
-    if(age.value <= 15)
-      {
-        saludo = "Hola pequeño "
-      }
-      else if (age.value > 15 && age.value <=35)
-      {
-        saludo = "Hola joven "
-      }
-      else
-      {
-        saludo = "Hola señor "
-      }
+  if (isNaN(user_age)) {
+    div.innerHTML = "<p>Por favor ingrese una edad válida.</p>";
+    return;
   }
-  else
-  {
-    if(age.value <= 15)
-      {
-        saludo = "Hola pequeña "
+
+  if (gender.value === "masculino") {
+    if (isEspañol) {
+      if (user_age <= 15) {
+        saludo = "Hola pequeño ";
+      } else if (user_age > 15 && user_age <= 35) {
+        saludo = "Hola joven ";
+      } else {
+        saludo = "Hola señor ";
       }
-      else if (age.value > 15 && age.value <=35)
-      {
-        saludo = "Hola señorita "
+    } else {
+      if (user_age <= 15) {
+        saludo = "Hello little boy ";
+      } else if (user_age > 15 && user_age <= 35) {
+        saludo = "Hello man ";
+      } else {
+        saludo = "Hello Sir. ";
       }
-      else
-      {
-        saludo = "Hola señora "
+    }
+  } else if (gender.value === "femenino") {
+    if (isEspañol) {
+      if (user_age <= 15) {
+        saludo = "Hola pequeña ";
+      } else if (user_age > 15 && user_age <= 35) {
+        saludo = "Hola señorita ";
+      } else {
+        saludo = "Hola señora ";
       }
+    } else {
+      if (user_age <= 15) {
+        saludo = "Hello little girl ";
+      } else if (user_age > 15 && user_age <= 35) {
+        saludo = "Hello baby girl ";
+      } else {
+        saludo = "Hello Lady  ";
+      }
+    }
+  } else {
+    div.innerHTML = "<p>Por favor ingrese un género válido.</p>";
+    return;
   }
-  
   
   div.innerHTML = "<p>" + saludo + user_Name + "</p>";
-});
+}
