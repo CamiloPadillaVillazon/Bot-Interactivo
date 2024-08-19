b3b316c7e5ff09222193448a4a003682f5a9c67b
Camilito <camilo.padilla@ucb.edu.bo>
2024-08-18 21:36:44 -0400

    Saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,6 @@
 
 const name = document.querySelector("#user-name")
+const gender = document.querySelector("#gender")
 const form = document.querySelector("#greeting-form");
 const div = document.querySelector("#Saludo-hola-div");
 
@@ -8,5 +9,14 @@ form.addEventListener("submit", (event) => {
 
   let saludo = "Hola "
   let user_Name = name.value
+  
+  if (gender.value === "masculino")
+  {
+    saludo = "Hola joven "
+  }else
+  {
+    saludo = "Hola señorita"
+  }
+    
   div.innerHTML = "<p>" + saludo + user_Name + "</p>";
 });
