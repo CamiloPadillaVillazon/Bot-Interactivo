31365d48e8dfe8f9634a2eaddfb1a89544b440d2
Camilito <camilo.padilla@ucb.edu.bo>
2024-08-18 21:20:09 -0400

    Saludar por Nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,12 @@
 
+const name = document.querySelector("#user-name")
 const form = document.querySelector("#greeting-form");
 const div = document.querySelector("#Saludo-hola-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let saludo = "Hola"
-  div.innerHTML = "<p>" + saludo + "</p>";
+  let saludo = "Hola "
+  let user_Name = name.value
+  div.innerHTML = "<p>" + saludo + user_Name + "</p>";
 });
